e313f319c93b60cd09396d82b997b4320414a40c
sohagmahin <sohagmahin@gmail.com>
2025-04-05 05:29:46 +0600

    add onDrag to upload font

--- src/components/font-input.tsx
+++ src/components/font-input.tsx
@@ -1,7 +1,10 @@
-import { useRef } from "react";
+import { cn } from "@/lib/utils";
+import { CloudUpload } from "lucide-react";
+import { useRef, useState } from "react";
 
 function FontInput({ setFonts }) {
   const fileInputRef = useRef(null);
+  const [isDragging, setIsDragging] = useState(false);
 
   const handleFontUpload = (e) => {
     const file = e.target.files[0];
@@ -23,9 +26,49 @@ function FontInput({ setFonts }) {
 
     setFonts((prev) => [...prev, newFont]);
   };
+
+  const handleOnDrop = (e) => {
+    e.preventDefault();
+    setIsDragging(false);
+    const file = e.dataTransfer.files[0];
+    if (file && !file.name.endsWith(".ttf")) {
+      alert("Only .ttf files are allowed");
+      return;
+    }
+
+    const fontName = file.name.replace(".ttf", "").replace(/\s/g, "");
+    const blobUrl = URL.createObjectURL(file);
+    const style = document.createElement("style");
+
+    style.innerHTML = `
+      @font-face {
+        font-family: '${fontName}';
+        src: url('${blobUrl}');
+      }
+    `;
+    document.head.appendChild(style);
+
+    const newFont = {
+      fontFamily: fontName,
+    };
+
+    setFonts((prev) => [...prev, newFont]);
+  };
+
   return (
     <section
-      className="flex flex-col justify-center items-center h-60 w-1/2 border-gray-300 border-1 shadow"
+      className={cn(
+        "flex flex-col justify-center items-center h-60 w-1/2 border-gray-200 border-1 shadow cursor-pointer",
+        {
+          "border-green-600 border-2": isDragging,
+        }
+      )}
+      onDrop={handleOnDrop}
+      onDragOver={(e) => {
+        e.preventDefault();
+        setIsDragging(true);
+      }}
+      onDragLeave={() => setIsDragging(false)}
       onClick={() => {
         fileInputRef.current?.click();
       }}
@@ -39,8 +82,11 @@ function FontInput({ setFonts }) {
         placeholder="Click to upload font"
       />
 
-      <span>Click to upload or drag and drop</span>
-      <span className="font-thin opacity-75 text-sm">
+      <CloudUpload color="#9EA4AF" size={30} />
+      <p className="opacity-70">
+        <span className="font-semibold">Click to upload</span> or drag and drop
+      </p>
+      <span className="font-normal opacity-50 text-sm">
         Only TTF File Allowed
       </span>
     </section>
